b159f20096ce04d83246b3f410fe7f23a4ef5fbf
zobirofkir <zobirofkir19@gmail.com>
2024-10-14 10:48:05 +0100

    change footer color

--- src/components/footer/FooterComponent.js
+++ src/components/footer/FooterComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FooterComponent = () => {
   return (
-    <footer className="bg-gray-800 text-white py-6">
+    <footer className="bg-white text-black py-6">
       <div className="flex flex-col items-center ">
         <h2 className="text-lg font-semibold">CSW SHOP</h2>
         <p>&copy; {new Date().getFullYear()} csw shop. All rights reserved.</p>
