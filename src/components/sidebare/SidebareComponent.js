750c4a37657c3263a626a80b69a4ec25366e1fac
zobirofkir <zobirofkir19@gmail.com>
2024-10-15 21:01:29 +0100

    create carts icon from sidebare

--- src/components/sidebare/SidebareComponent.js
+++ src/components/sidebare/SidebareComponent.js
@@ -87,6 +87,57 @@ const SidebareComponent = () => {
           <a href='/offers'>Offers</a>
         </div>
 
+        <div
+          role="button"
+          className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+        >
+          <div className="grid mr-4 place-items-center">
+            {/* Carts Icon */}
+            <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  fill="none" 
+                  viewBox="0 0 24 24" 
+                  stroke-width="1.5" 
+                  stroke="currentColor" 
+                  aria-hidden="true" 
+                  class="w-5 h-5"
+              >
+                  <g id="SVGRepo_iconCarrier">
+                      <path 
+                          d="M2 3L2.26491 3.0883C3.58495 3.52832 4.24497 3.74832 4.62248 4.2721C5 4.79587 5 5.49159 5 6.88304V9.5C5 12.3284 5 13.7426 5.87868 14.6213C6.75736 15.5 8.17157 15.5 11 15.5H19" 
+                          stroke="currentColor" 
+                          stroke-width="1.5" 
+                          stroke-linecap="round" 
+                          stroke-linejoin="round"
+                      />
+                      <path 
+                          d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z" 
+                          stroke="currentColor" 
+                          stroke-width="1.5" 
+                          stroke-linecap="round" 
+                          stroke-linejoin="round"
+                      />
+                      <path 
+                          d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z" 
+                          stroke="currentColor" 
+                          stroke-width="1.5" 
+                          stroke-linecap="round" 
+                          stroke-linejoin="round"
+                      />
+                      <path 
+                          d="M5 6H16.4504C18.5054 6 19.5328 6 19.9775 6.67426C20.4221 7.34853 20.0173 8.29294 19.2078 10.1818L18.7792 11.1818C18.4013 12.0636 18.2123 12.5045 17.8366 12.7523C17.4609 13 16.9812 13 16.0218 13H5" 
+                          stroke="currentColor" 
+                          stroke-width="1.5" 
+                          stroke-linecap="round" 
+                          stroke-linejoin="round"
+                      />
+                  </g>
+              </svg>
+
+          </div>
+          <a href='/carts'>Carts</a>
+        </div>
+
       </nav>
 
       {/* Conditional Rendering for Login/Register or Logout */}
